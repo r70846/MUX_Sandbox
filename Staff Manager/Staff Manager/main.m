3612a5aa00ca2f61f06e889ce872d526912be853
Russ Gaspard <russg@fullsail.edu>
2014-05-17 12:23:36 -0400

    initial files added

---
+++ Staff Manager/Staff Manager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Staff Manager
+//
+//  Created by Russell Gaspard on 5/17/14.
+//  Copyright (c) 2014 Russell Gaspard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
