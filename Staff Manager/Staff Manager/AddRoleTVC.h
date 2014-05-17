09899543873f06efd5195a380770ac6c01803504
Russ Gaspard <russg@fullsail.edu>
2014-05-17 13:12:47 -0400

    built through pg7

---
+++ Staff Manager/Staff Manager/AddRoleTVC.h
@@ -0,0 +1,13 @@
+//
+//  AddRoleTVC.h
+//  Staff Manager
+//
+//  Created by Russell Gaspard on 5/17/14.
+//  Copyright (c) 2014 Russell Gaspard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddRoleTVC : UITableViewController
+
+@end
