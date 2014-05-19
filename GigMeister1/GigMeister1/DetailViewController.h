a990c8082cde2cd3e2c571e97cbfd4d1c0e66513
Russ Gaspard <russg@fullsail.edu>
2014-05-18 22:23:47 -0400

    finished week2 app

--- GigMeister1/GigMeister1/DetailViewController.h
+++ GigMeister1/GigMeister1/DetailViewController.h
@@ -5,6 +5,14 @@
 //  Created by Russell Gaspard on 5/17/14.
 //  Copyright (c) 2014 Russell Gaspard. All rights reserved.
 //
+/*
+ 
+ Russ Gaspard
+ Week 2
+ Mobile Development
+ MUX 1405
+ 
+ */
 
 #import <UIKit/UIKit.h>
 #import "GigDateClass.h"
